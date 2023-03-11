0e767ff9975b47b1bd042cd9921bae1253d02d48
Ronald Barasa <barasaogoro@gmail.com>
2023-03-11 04:32:35 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    if (b === 0) {
+      return "Error: cannot divide by zero";
+    }
+    return a / b;
+  }
+  function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  function makeInt(string) {
+    return parseInt(string, 10);
+  }
+  
+  function preserveDecimal(string) {
+    return parseFloat(string);
+  }
+  
